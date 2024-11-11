1e9ae85ab2dadc3e147e1337064619b76cb3947d
ThienDoNgoc <haithaobathientuthanh@gmail.com>
2024-11-11 19:52:18 +0700

    update UserResponse

--- Fvent.Service/Result/UserResponse.cs
+++ Fvent.Service/Result/UserResponse.cs
@@ -4,6 +4,6 @@ public record GetListUserRes(string Username, string AvatarUrl, string Email,
                              string PhoneNumber, string CardUrl, VerifiedStatus Verified, string RoleName, DateTime CreatedAt,
                              DateTime? UpdatedAt, bool IsDeleted, DateTime? DeletedAt);
 
-public record UserRes(Guid UserId, string Username, string AvatarUrl, string Email, string PhoneNumber, string CardUrl, string RoleName);
+public record UserRes(Guid UserId, string Username, string AvatarUrl, string Email, string PhoneNumber, string CardUrl, string VerifyStatus,string ProcessNote, string RoleName);
 
 public record AuthResponse(string Token, string RefreshToken);
